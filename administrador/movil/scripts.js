c00a7adc6b2af4b204efc758b61e1afd951897a7
jvalderrama2602 <jvalderrama2602@gmail.com>
2017-05-12 06:42:37 -0400

    Actualizacion 12-05-2017

--- administrador/movil/scripts.js
+++ administrador/movil/scripts.js
@@ -1,417 +1,277 @@
-
-
-  function actualizar(x){
-
-    switch (x) {
-
+function actualizar(a) {
+    switch (a) {
       case 0:
-
-      document.getElementById('x1').style.display = 'none';
-
-      document.getElementById('x2').style.display = 'none';
-
-      document.getElementById('grabar').style.display = '';
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      location.reload(true);
-
-      break;
+        document.getElementById("x1").style.display = "none";
+        document.getElementById("x2").style.display = "none";
+        document.getElementById("grabar").style.display = "";
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        location.reload(true);
+        break;
 
       case 1:
-
-      document.getElementById('x1').style.display = 'none';
-
-      document.getElementById('x2').style.display = 'none';
-
-      document.getElementById('grabar').style.display = '';
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      location.reload(true);
-
-      break;
+        document.getElementById("x1").style.display = "none";
+        document.getElementById("x2").style.display = "none";
+        document.getElementById("grabar").style.display = "";
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        location.reload(true);
+        break;
 
       case 2:
-
-      document.getElementById('x1').style.display = 'none';
-
-      document.getElementById('x2').style.display = 'none';
-
-      document.getElementById('grabar').style.display = '';
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      location.reload(true);
-
-      break;
+        document.getElementById("x1").style.display = "none";
+        document.getElementById("x2").style.display = "none";
+        document.getElementById("grabar").style.display = "";
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        location.reload(true);
+        break;
 
       case 3:
-
-      document.getElementById('x1').style.display = 'none';
-
-      document.getElementById('x2').style.display = 'none';
-
-      document.getElementById('grabar').style.display = '';
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      location.reload(true);
-
-      break;
+        document.getElementById("x1").style.display = "none";
+        document.getElementById("x2").style.display = "none";
+        document.getElementById("grabar").style.display = "";
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        location.reload(true);
+        break;
 
       default:
-
-      alert("alert");
-
+        alert("alert");
     }
-
-  }
-
-
-
-  function mostrar(x){
-
-    switch (x) {
-
-      case 0:
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-
-
-      break;
-
-      case 1:
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      break;
-
-      case 2:
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='none';
-
-      break;
-
-      case 3:
-
-      div=document.getElementById("wrapper2");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper3");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper4");
-
-      div.style.display='none';
-
-      div=document.getElementById("wrapper5");
-
-      div.style.display='';
-
-      break;
-
-      default:
-
-      alert("alert");
-
-    }
-
-  }
-
-
-
-
-
-function validar(e) { // 1
-
-    tecla = (document.all) ? e.keyCode : e.which; // 2
-
-    if (tecla==8) return true; // 3
-
-    patron =/[A-Za-z\s]/; // 4
-
-    te = String.fromCharCode(tecla); // 5
-
-    return patron.test(te); // 6
-
 }
 
+function mostrar(a) {
+    switch (a) {
+      case 0:
+        div = document.getElementById("wrapper2");
+        div.style.display = "";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        break;
 
+      case 1:
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        break;
 
-function getXMLHTTPRequest()
+      case 2:
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "";
+        div = document.getElementById("wrapper5");
+        div.style.display = "none";
+        break;
 
-{
+      case 3:
+        div = document.getElementById("wrapper2");
+        div.style.display = "none";
+        div = document.getElementById("wrapper3");
+        div.style.display = "none";
+        div = document.getElementById("wrapper4");
+        div.style.display = "none";
+        div = document.getElementById("wrapper5");
+        div.style.display = "";
+        break;
 
-	try { req = new XMLHttpRequest(); }
+      default:
+        alert("alert");
+    }
+}
 
-	catch(err1)
+function validar(a) {
+    tecla = document.all ? a.keyCode : a.which;
+    if (8 == tecla) return true;
+    patron = /[A-Za-z\s]/;
+    te = String.fromCharCode(tecla);
+    return patron.test(te);
+}
 
-	{
-
-  		try { req = new ActiveXObject("Msxml2.XMLHTTP"); }
-
-		catch (err2)
-
-		{try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (err3) { req = false; }}
-
-	}
-
-	return req;
-
+function getXMLHTTPRequest() {
+    try {
+        req = new XMLHttpRequest();
+    } catch (a) {
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (b) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (c) {
+                req = false;
+            }
+        }
+    }
+    return req;
 }
 
 var http = getXMLHTTPRequest();
-  function enviarHTTP01 ()
+
+function enviarHTTP01() {
+    var a = window.document.formulario_usuario01.nombre.value;
+    if ("" != a) {
+        var b = "usuario=" + a;
+        http.open("GET", "../categoria/grabar_categoria.php?" + b, true);
+        http.onreadystatechange = respuestaHTTP01;
+        http.send(null);
+    }
+}
+
+function enviarHTTP02() {
+    var a = window.document.formulario_usuario02.tipo.value;
+    var b = window.document.formulario_usuario02.nombre.value;
+    if (0 != a && "" != b) {
+        var c = "estado=" + a + "&nombre=" + b;
+        http.open("GET", "../ciudad/grabar_ciudad.php?" + c, true);
+        http.onreadystatechange = respuestaHTTP02;
+        http.send(null);
+    }
+}
+
+function enviarHTTP03() {
+    var a = window.document.formulario_usuario03.nombre.value;
+    if ("" != a) {
+        var b = "usuario=" + a;
+        http.open("GET", "../estado/grabar_estados.php?" + b, true);
+        http.onreadystatechange = respuestaHTTP03;
+        http.send(null);
+    }
+}
+
+function enviarHTTP04() {
+    var a = document.getElementById("test3").value;
+    var b = document.getElementById("nombre").value;
+    var c = document.getElementById("apellido").value;
+    var d = document.getElementById("email").value;
+    var e = document.getElementById("login").value;
+    var f = document.getElementById("password").value;
+    var g = document.getElementById("confirmacion").value;
+    var h = 0;
+    if (f == g) {
+        var i = "tipo=" + a + "&nombre=" + b + "&apellido=" + c + "&email=" + d + "&login=" + e + "&pass=" + f + "&confirmacion=" + g;
+        http.open("GET", "../super_usuario/grabar_usuario.php?" + i, true);
+        http.onreadystatechange = respuestaHTTP04;
+        http.send(null);
+    } else alert("No son iguales el pass");
+}
+
+
+function enviarHTTP05 ()
+{
+  var nombre =document.getElementById("nombrep").value;
+  var categoria =document.getElementById("category").value;
+  //var nombre = window.document.formulario_usuario05.nombrep.value;
+  //var categoria = window.document.formulario_usuario05.category.value;
+
+  if(nombre!="" && categoria!=0)
   {
-    var valor = window.document.formulario_usuario01.nombre.value;
-    if(valor!="")
-    {
-      var query= "usuario=" + valor;
-      http.open ("GET", "../categoria/grabar_categoria.php?" + query, true);
-      http.onreadystatechange = respuestaHTTP01;
+    var query= "nombre=" + nombre +"&categoria=" + categoria;
+    http.open ("GET", "../tipo_producto/grabar_tipo_producto.php?" + query, true);
+    http.onreadystatechange = respuestaHTTP05;
     http.send(null); // se envia la petición
   }
 }
 
-function enviarHTTP02 ()
-{
-	var estado = window.document.formulario_usuario02.tipo.value;
-	var nombre = window.document.formulario_usuario02.nombre.value;
-	if(estado!=0 && nombre!="")
-	{
-		var query= "estado=" + estado +"&nombre=" + nombre;
-		http.open ("GET", "../ciudad/grabar_ciudad.php?" + query, true);
-		http.onreadystatechange = respuestaHTTP02;
-		http.send(null); // se envia la petición
-	}
+function respuestaHTTP01() {
+    if (4 == http.readyState) if (200 == http.status) {
+        var a = http.responseText;
+        if ("" == a) Materialize.toast("Atención Registro ya existe", 3e3); else {
+            Materialize.toast("Registro correcto", 3e3);
+            window.document.formulario_usuario01.nombre.value = "";
+            location.reload(true);
+        }
+    }
 }
 
-function enviarHTTP03 ()
-{
-  var valor = window.document.formulario_usuario03.nombre.value;
-  if(valor!="")
-  {
-    var query= "usuario=" + valor;
-    http.open ("GET", "../estado/grabar_estados.php?" + query, true);
-    http.onreadystatechange = respuestaHTTP03;
-    http.send(null); // se envia la petición
-  }
+function respuestaHTTP02() {
+    if (4 == http.readyState) if (200 == http.status) {
+        var a = http.responseText;
+        if ("" == a) Materialize.toast("Atención Registro ya existe", 3e3); else {
+            Materialize.toast("Registro correcto", 3e3);
+            window.document.formulario_usuario02.nombre.value = "";
+            location.reload(true);
+        }
+    }
 }
 
-function enviarHTTP04 ()
-{
-  var tipo = document.getElementById("test3").value;
-  var nombre = document.getElementById("nombre").value;
-  var apellido = document.getElementById("apellido").value;
-  var email = document.getElementById("email").value;
-  var login = document.getElementById("login").value;
-  var pass = document.getElementById("password").value;
-  var confirmacion = document.getElementById("confirmacion").value;
-  var cont=0;
-
-
-
-  if(pass==confirmacion)
-  {
-
-    var query="tipo=" + tipo + "&nombre=" + nombre +"&apellido=" + apellido +"&email=" + email +"&login=" + login +"&pass=" + pass +"&confirmacion=" + confirmacion;
-    http.open ("GET", "../super_usuario/grabar_usuario.php?" + query, true);
-    http.onreadystatechange = respuestaHTTP04;
-    http.send(null); // se envia la petición
-  }
-  else
-  {alert('No son iguales el pass')}
+function respuestaHTTP03() {
+    if (4 == http.readyState) if (200 == http.status) {
+        var a = http.responseText;
+        if ("" == a) Materialize.toast("Atención Registro ya existe", 3e3); else {
+            Materialize.toast("Registro correcto", 3e3);
+            window.document.formulario_usuario03.nombre.value = "";
+            location.reload(true);
+        }
+    }
 }
 
+function respuestaHTTP04() {
+    if (4 == http.readyState) if (200 == http.status) {
+        var a = http.responseText;
 
-function respuestaHTTP01 ()
+        if (0 != a) {
+            Materialize.toast("Se Ha Registrado Correctamente", 3e3);
+            location.href = "usuarios.php";
+        } else {
+            Materialize.toast("NO Se Ha Registrado", 3e3);
+            location.href = "usuarios.php";
+        }
+    }
+}
+
+function respuestaHTTP05 ()
 {
   if (http.readyState == 4)
   {
     if (http.status == 200)
     {
       var respuesta_php = http.responseText;
+
       if(respuesta_php=="")
       {
-        Materialize.toast('Atención Registro ya existe', 3000);
+         Materialize.toast("Producto ya existe", 3e3);
+            location.href = "producto.php";
       }
       else
       {
-        Materialize.toast('Registro correcto', 3000);
-        window.document.formulario_usuario01.nombre.value="";
-        location.reload(true);
-      }
-    }
-  }
-}
-
-
-function respuestaHTTP02 ()
-{
-  if (http.readyState == 4)
-  {
-    if (http.status == 200)
-    {
-      var respuesta_php = http.responseText;
-      if(respuesta_php=="")
-      {
-        Materialize.toast('Atención Registro ya existe', 3000);
-      }
-      else
-      {
-        Materialize.toast('Registro correcto', 3000);
-        window.document.formulario_usuario02.nombre.value="";
-        location.reload(true);
-      }
-    }
-  }
-}
-
-function respuestaHTTP03 ()
-{
-
-  if (http.readyState == 4)
-  {
-    if (http.status == 200)
-    {
-      var respuesta_php = http.responseText;
-      if(respuesta_php=="")
-      {
-        Materialize.toast('Atención Registro ya existe', 3000);
-      }
-      else
-      {
-        Materialize.toast('Registro correcto', 3000);
-        window.document.formulario_usuario03.nombre.value="";
-        location.reload(true);
-      }
-    }
-  }
-}
-
-function respuestaHTTP04 ()
-{
-  if (http.readyState == 4)
-  {
-    if (http.status == 200)
-    {
-      var respuesta_php = http.responseText;
-
-      alert(respuesta_php);
-
-      if(respuesta_php!=0)
-      {
-
-        Materialize.toast('Se Ha Registrado Correctamente', 3000);
-        location.href="usuarios.php";
-      }
-      else
-      {
-
-      Materialize.toast('NO Se Ha Registrado', 3000);
-      location.href="usuarios.php";
+        Materialize.toast("Producto Registrado", 3e3);
+            location.href = "producto.php";
       }
     }
   }
