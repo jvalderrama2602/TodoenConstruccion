6b4abacb3da7b899070c4cd5d8979d975449fb2e
jvalderrama2602 <jvalderrama2602@gmail.com>
2017-05-29 20:48:26 -0400

    Opciones de mi cuente

--- mi_cuenta.php
+++ mi_cuenta.php
@@ -2,6 +2,45 @@
 include('header.php');
 ?>
 
+<div class="content-container no-padding">
+				<div class="container-full">
+					<div class="main-content">
+						<div class="container">
+							<div class="row shipping-policy">
+								<div class="policy-featured-col col-md-4 col-sm-6">
+									<i class="fa fa-money"></i>
+									<h4 class="policy-featured-title">
+										Agregar <br> Producto para Vender
+									</h4>
+									Publica un producto nuevo o usado
+								</div>
+								<div class="policy-featured-col col-md-4 col-sm-6">
+									<i class="fa fa-globe"></i>
+									<h4 class="policy-featured-title">
+										Responde <br> las preguntas de tus interesados
+									</h4>
+									Respondele rápido y sé amable
+								</div>
+								<div class="policy-featured-col col-md-4 col-sm-6">
+									<i class="fa fa-clock-o"></i>
+									<h4 class="policy-featured-title">
+										Agregar <br> Servicios Profesionales
+									</h4>
+									Ofrezca sus servicios aqui.
+								</div>
+							</div>
+						</div>
+
+
+
+
+
+					</div>
+				</div>
+			</div>
+
+
+
 <table align="center" border="0">
 <tr>
     <td><a href="agregar_venta.php">Agregar Producto para Venta</a></td>
